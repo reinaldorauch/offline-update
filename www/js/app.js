5de442ac9bbd40be9a487dca6963a2a4b3314565
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2015-10-26 19:21:55 -0200

    Arrumado estilo de codificação do app.js

--- www/js/app.js
+++ www/js/app.js
@@ -6,9 +6,14 @@
   // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
   // the 2nd parameter is an array of 'requires'
   // 'starter.controllers' is found in controllers.js
-  angular.module('starter', ['ionic', 'starter.controllers'])
+  var starter = angular.module('starter', ['ionic', 'starter.controllers']);
 
-  .run(function($ionicPlatform) {
+  starter.run(bootstrap)
+    .config(states);
+
+  bootstrap.$inject = ['$ionicPlatform'];
+
+  function bootstrap ($ionicPlatform) {
     $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -22,28 +27,29 @@
         StatusBar.styleDefault();
       }
     });
-  })
+  }
 
-  .config(function($stateProvider, $urlRouterProvider) {
+  states.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function states($stateProvider, $urlRouterProvider) {
     $stateProvider
-
       .state('app', {
-      url: '/app',
-      abstract: true,
-      templateUrl: 'templates/menu.html',
-      controller: 'AppCtrl'
-    })
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
 
-    .state('app.search', {
-      url: '/search',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/search.html'
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
         }
-      }
-    })
+      })
 
-    .state('app.browse', {
+      .state('app.browse', {
         url: '/browse',
         views: {
           'menuContent': {
@@ -51,26 +57,28 @@
           }
         }
       })
+
       .state('app.playlists', {
         url: '/playlists',
+          views: {
+            'menuContent': {
+              templateUrl: 'templates/playlists.html',
+              controller: 'PlaylistsCtrl'
+            }
+          }
+        })
+
+      .state('app.single', {
+        url: '/playlists/:playlistId',
         views: {
           'menuContent': {
-            templateUrl: 'templates/playlists.html',
-            controller: 'PlaylistsCtrl'
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
           }
         }
-      })
+      });
 
-    .state('app.single', {
-      url: '/playlists/:playlistId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlist.html',
-          controller: 'PlaylistCtrl'
-        }
-      }
-    });
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/add');
-  });
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/app/add');
+  }
 })();
